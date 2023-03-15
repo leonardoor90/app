c82b843d2d5377c544e6affade0e38b73ce5f533
leonardoor90 <leoor90@gmail.com>
2023-03-15 07:59:03 -0300

    manipulação states

--- src/components/search/index.jsx
+++ src/components/search/index.jsx
@@ -1,9 +1,21 @@
+import { useState } from 'react';
 import './styles.css'
 
 const Search = () =>{
+
+    const[inputValue, setInputValue] = useState('')
+
+    const handleInputvalue = (event)=>{
+        const {value} = event.target;
+
+        setInputValue(value)
+    }
+
+    console.log(inputValue);
+
     return(
         <form className="Search">
-            <input name="search" placeholder="Search Recipes" id="search"/>
+            <input name="search" onChange={handleInputvalue} value={inputValue} placeholder="Search Recipes" id="search"/>
             <button type="submit">Search</button>
 
         </form>
